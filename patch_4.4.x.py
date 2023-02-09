5b3a2e811e5ec898afbae81dc428a192cf86dc9b
Shan_shanHY <shan_shanhy@qq.com>
2023-02-09 16:58:32 +0800

    Update patch_4.4.x.py

--- patch_4.4.x.py
+++ patch_4.4.x.py
@@ -1,6 +1,5 @@
 patch=[
-'''
-	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
+'''	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
 	int rc;
 
@@ -8,10 +7,8 @@ patch=[
 		return 0; /* neither NNP nor nosuid */
 
 	if (new_tsec->sid == old_tsec->sid)
-		return 0; /* No change in credentials */
-''',
-'''
-	static u32 ksu_sid;
+		return 0; /* No change in credentials */''',
+'''	static u32 ksu_sid;
 	char *secdata;
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
@@ -35,22 +32,22 @@ patch=[
 		if(rc == 0 && new_tsec->sid == ksu_sid){
 			return 0;
 		}
-	}
-''']
+	}''']
 
 try:
-	with open('security/selinux/hooks.c}','r',encoding = 'utf-8') as f:
+	with open('security/selinux/hooks.c','r',encoding = 'utf-8') as f:
 		code=f.read()
-	if code.find(patch[2]) == -1:
-		if code.find(patch[1]) != -1:
-			patched_code = code.replace(patch[1], patch[2])
-			with open('security/selinux/hooks.c','w',encoding = 'utf-8') as f:
-				f.write(patched_code)
-			print('Patch Selinux Successfully!')
-		else:
-			print('Patch Selinux Failed!')
-	else:
+	if code.find(patch[1]) != -1:
 		print('File Selinux has been patched!')
+		exit(0)
+	if code.find(patch[0]) != -1:
+		patched_code = code.replace(patch[0], patch[1])
+		with open('security/selinux/hooks.c','w',encoding = 'utf-8') as f:
+			f.write(patched_code)
+		print('Patch Selinux Successfully!')
+	else:
+		print('Patch Selinux Failed!')
+		exit(-1)
 except:
 	print('Patch Failed! Nothing could patch!')
 	exit(-1)
